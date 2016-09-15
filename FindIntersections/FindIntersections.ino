e0ad138e1642cecf72f80a6e430f7502d88a3483
Seth Troisi <braintwo@gmail.com>
2016-09-14 23:47:03 -0700

    Added snakes v1

--- FindIntersections/FindIntersections.ino
+++ FindIntersections/FindIntersections.ino
@@ -4,14 +4,13 @@
 #define NUM_LIGHTS 150
 #define BRIGHTNESS 32
 
-#define DELAY_MS 65
+#define DELAY_MS 25
 
 #define num_elements(x)  (sizeof(x) / sizeof((x)[0]))
 
 int intersections[][6] = {
-  {  15,  16},
+  {  15,  16,  91,  92},
   {  29,  30, 107, 108, 109},
-  {  91,  92},
 };
 
 
@@ -33,7 +32,6 @@ uint32_t PURPLE = strip.Color(32, 0, 88);
 uint32_t BLACK  = strip.Color(0, 0, 0);
 uint32_t WHITE  = strip.Color(255, 255, 255);
 
-
 uint32_t fun_colors[] = {RED, GREEN, BLUE, PURPLE, BLACK, WHITE};
 
 void setup() {
@@ -48,12 +46,8 @@ void loop() {
     int num_intersections = num_elements(intersections);
     for (int i_index = 0; i_index < num_intersections; i_index += 1) {
       int *intersection_points = intersections[i_index];
-      for (int j = 0; true; j++) {
-        int led_index = intersection_points[j];
-        if (led_index <= 0) {
-          break;
-        }
-        
+      for (int j = 0; intersection_points[j] > 0; j++) {
+        int led_index = intersection_points[j];        
         strip.setPixelColor(led_index, fun_colors[(f + i_index + j) % num_elements(fun_colors)]);
       }
     }
@@ -71,10 +65,36 @@ void loop() {
 
   for (int f = 0; f < 10000; f++) {
     int num_snakes = num_elements(snakes);
-    for (int snake_index = 0; snake_index < 1; snake_index += 1) {
-      strip.setPixelColor(snakes[snake_index], BLACK);
-      snakes[snake_index] = (snakes[snake_index] + 1) % NUM_LIGHTS;
-      strip.setPixelColor(snakes[snake_index], fun_colors[snake_index]);
+    for (int snake_index = 0; snake_index < num_snakes; snake_index += 1) {
+      int current_led = snakes[snake_index];
+
+      strip.setPixelColor(current_led, BLACK);
+
+      int next_led = (current_led + 1) % NUM_LIGHTS;
+
+      // see if this is an intersection
+      int n_i = num_elements(intersections);
+      for (int i_index = 0; i_index < n_i; i_index += 1) {
+        int *i_points = intersections[i_index];
+        for (int j = 0; i_points[j] > 0; j++) {
+          int led_index = i_points[j];
+
+          if (led_index == current_led) {
+            // This led is part of an intersection
+            // 50% of the time jump to one of the other LEDS in the intersection 
+            if (random(0,2) == 0) {
+              if (j == 0) {
+                next_led = i_points[1] + 2; // move out of the intersection
+              } else {
+                next_led = i_points[j - 1] + 2;
+              }
+            }
+          }
+        }
+      }
+
+      strip.setPixelColor(next_led, fun_colors[snake_index]);
+      snakes[snake_index] = next_led;
     }
 
     strip.show();
