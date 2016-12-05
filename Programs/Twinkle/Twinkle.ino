c6f4d2df60506916712685e394003742e0bc43c9
erindelaney <erinkdelaney@gmail.com>
2016-12-04 16:49:47 -0800

    Fix Twinkle

--- Programs/Twinkle/Twinkle.ino
+++ Programs/Twinkle/Twinkle.ino
@@ -8,11 +8,11 @@
 #define BRIGHTNESS  LIGHT_CONFIG_BRIGHTNESS
 
 // TODO(ERIN): move to LIGHT_CONFIG
-#define NUM_TWINKLES 15
 #define DELAY_MS    120
 
 #define num_elements(x)  (sizeof(x) / sizeof((x)[0]))
 
+double density = 0.03; //average percent of lights turned on
 char twinkle_location[NUM_LIGHTS] = {};
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LIGHTS, PIN_NUMBER, NEO_GRB + NEO_KHZ800);
@@ -25,35 +25,19 @@ void setup() {
   strip.show(); // Initialize all pixels to 'off'
 }
 
-int iterations = 0;
-void loop() {
-  if (iterations == 20) {
-      // Set a "twinkle" at random points on twinkle_location for each pass of loop
-      int set_twinkle = random(NUM_LIGHTS);
-      twinkle_location[set_twinkle] = 19;
-      iterations = 0;
-    }
-    else {
-      iterations += 1;
-    }
 
-//  for (int t = 0; t < random(2 * NUM_TWINKLES); t += 1) {
-//    if (iterations == 20) {
-//      // Set a "twinkle" at random points on twinkle_location for each pass of loop
-//      int set_twinkle = random(NUM_LIGHTS);
-//      twinkle_location[set_twinkle] = 19;
-//      iterations = 0;
-//    }
-//    else {
-//      iterations += 1;
-//    }
-//
-//  }
+void loop() {
+
+  for (int x = 0; x < NUM_LIGHTS; x++) {
+    if (random(10000) < density / 20 * 10000 && twinkle_location[x] == 0) {
+      twinkle_location[x] = 19;
+    }
+  }
 
   for (int light = 0; light < NUM_LIGHTS; light++) {
     if (twinkle_location[light] > 0) {
-      uint32_t picker = light % (num_elements(cool_colors));
-      uint32_t light_color = cool_colors[picker];
+      uint32_t picker = light % (num_elements(xmas_colors));
+      uint32_t light_color = xmas_colors[picker];
       uint32_t fade_color;
       if (twinkle_location[light] > 15) {
         fade_color = LinearColorFade(BLACK, light_color, pow(((20 - twinkle_location[light])) / 5.0, 2));
@@ -72,6 +56,7 @@ void loop() {
   strip.show();
   delay(DELAY_MS);
 }
+
 
 
 // Return colors that is a blend of a and b.
